b02b8cef2e16e037d62d915fe209a8652abfa12b
Ne0bliviscaris <133912560+Ne0bliviscaris@users.noreply.github.com>
2023-12-06 18:18:13 +0100

    Finished

--- Zadania domowe/2/11.R
+++ Zadania domowe/2/11.R
@@ -2,52 +2,15 @@
 # Zadanie.11
 
 # Wczytaj dane ze zbioru "countries of the world.csv" 
-# Zapoznaj się z danymi korzystając np. z funkcji names(), summary(), str(), 
-# dim(), itd., obejrzysz też dane za pomocą funkcji View()
-# Zamień puste pola na wartości NA: data[data==""] <- NA
-# zmień nazwy kolumn, zgodnie z następującym wzorem:
-# Area_sq_mi -> Area
-# Pop_Density_per_sq_mi -> Pop_dens
-# GDPper_capita -> GDP
-# Usuń ze zbioru wszystkie zmienne poza: Country, Region, Population, Area, 
-# Pop_dens, GDP, Literacy, Birthrate, Deathrate.
-# Usuń spacje ze zmiennej Region (z przodu i z tyłu nazw - funkcja str_trim)
-#  
-# Dla ilu krajów występują braki danych w zmiennej Literacy? Użyj funkcji is.na i sum
-# Wyświetl wszystkie kategorie zmiennej Region. Jaka jest dominanta? (RVAideMemoire::mod(?))
-# Sprawdź udział procentowy poszczególnych kategorii.
-# Jaka jest średnia i mediana zmiennych Population, Area i GDP? 
-# Stwórz histogram oraz wykres pudełkowy dla zmiennych GDP oraz Literacy. 
-# Odpowiednio podpisz osie histogramu (uwzględniając też jednostki)
-# Wyświetl kwartyle dla funkcji GDP. Teraz stwórz nową zmienną - będzie to zmienna 
-# kategoryczną oparta na zmiennej GDP. Nowa zmienna:
-# - powinna się nazywać "GDP_binned"
-# - będzie przyjmować cztery wartości (kategorie):
-# - pierwsza kategoria ma zawierać wartości od minimum do pierwszego kwartyla. Nazwij ją "Very_poor"
-# - druga kategoria ma zawierać wartości od pierwszego kwartyla do mediany. Nazwij ją "Poor"
-# - trzecia kategoria ma zawierać wartości od mediany do trzeciego. Nazwij ją "Medium"
-# - czwarta kategoria ma zawierać wartości od mediany do trzeciego. Nazwij ją "Rich"
-# - (użyj funkcji ifelse albo case_when)  
-# Stwórz nową zmienną logiczną, która przyjmuje wartość „True”, kiedy wartość 
-# zmiennej „Birthrate” jest większa lub równa od wartości zmiennej „Deathrate”,
-# a wartość „False” w przeciwnym przypadku. Wartość „True” w nowej zmiennej
-# oznacza – najprościej ujmując – że w roku badania w danym kraju więcej osób się
-# urodziło niż umarło. Stwórz wykres słupkowy dla nowo powstałej zmiennej, 
-#  pokazując częstości występowania pierwszej i drugiej kategorii.
-
-
-# Wczytaj dane ze zbioru "countries of the world.csv" 
-link <- "https://github.com/RPuakowski/Teaching/raw/main/countries%20of%20the%20world.csv"
-df <- read.csv(link, header = TRUE, sep = ",", dec = ",")	
-print(df)
+df <- read.csv("https://github.com/RPuakowski/Teaching/raw/main/countries%20of%20the%20world.csv", header = TRUE, sep = ",", dec = ",")	
 # Zapoznaj się z danymi korzystając np. z funkcji names(), summary(), str(), dim(), itd., obejrzysz też dane za pomocą funkcji View()
 names(df)
 summary(df)
 str(df)
 dim(df)
-View(df)
+#View(df) - ukryte dla szybkiego ponownego wykonywania całości
 # Zamień puste pola na wartości NA: data[data==""] <- NA
-df <- read.csv(link, na.strings = c("", "NA"))
+df <- read.csv("https://github.com/RPuakowski/Teaching/raw/main/countries%20of%20the%20world.csv", na.strings = c("", "NA"))
 # zmień nazwy kolumn, zgodnie z następującym wzorem:
 # Area_sq_mi -> Area
 colnames(df) <- gsub("Area..sq..mi..", "Area", colnames(df))
@@ -62,12 +25,6 @@ df <- subset(df, select = c("Country", "Region","Population", "Area", "Pop_dens"
 # Usuń spacje ze zmiennej Region (z przodu i z tyłu nazw - funkcja str_trim)
 df$Region <- stringr::str_trim(df$Region)
 
-
-
-
-
-
-
 # Dla ilu krajów występują braki danych w zmiennej Literacy? Użyj funkcji is.na i sum
 cat("Braki w literacy występują dla", sum(is.na(df$Literacy)), "krajów")
 # Wyświetl wszystkie kategorie zmiennej Region. Jaka jest dominanta? (RVAideMemoire::mod(?))
@@ -80,24 +37,43 @@ cat("Dla zmiennej Population średnia wynosi:", mean(df$Population),", a mediana
 cat("Dla zmiennej Area średnia wynosi:", mean(df$Area),", a mediana wynosi:", median(df$Area))
 cat("Dla zmiennej GDP średnia wynosi:", mean((df$GDP), na.rm = TRUE),", a mediana wynosi:", median((df$GDP), na.rm = TRUE))
 # Stwórz histogram oraz wykres pudełkowy dla zmiennych GDP  oraz Literacy.
-hist(df$GDP, main = "Global GDP", xlab = "Billions usd", ylab = "Countries", col = "green", labels = TRUE )
-boxplot(df$GDP, main = "Global GDP", ylab = "Billions usd", col = "green", range = 1.4)
+hist(df$GDP, main = "Global GDP", xlab = "Millions usd", ylab = "Countries", col = "lightgreen", labels = TRUE )
+boxplot(df$GDP, main = "Global GDP", ylab = "Millions usd", col = "lightgreen", range = 1.4)
 # Odpowiednio podpisz osie histogramu (uwzględniając też jednostki)
 df$Literacy <- gsub(",", ".", df$Literacy)
 hist(na.omit(as.numeric(df$Literacy)), main = "Global Literacy", xlab = "Literacy %", ylab = "Countries", col = "brown", labels = TRUE )
 boxplot(as.numeric(df$Literacy), main = "Global Literacy", ylab = "Literacy %", col = "brown", range = 1.2)
-# Wyświetl kwartyle dla funkcji GDP. Teraz stwórz nową zmienną - będzie to zmienna 
+# Wyświetl kwartyle dla funkcji GDP.
+quantile(df$GDP, na.rm = T)
+# Teraz stwórz nową zmienną - będzie to zmienna 
 # kategoryczną oparta na zmiennej GDP. Nowa zmienna:
 # - powinna się nazywać "GDP_binned"
 # - będzie przyjmować cztery wartości (kategorie):
 # - pierwsza kategoria ma zawierać wartości od minimum do pierwszego kwartyla. Nazwij ją "Very_poor"
+first_quartile <- quantile(df$GDP, 0.25, na.rm = TRUE)
 # - druga kategoria ma zawierać wartości od pierwszego kwartyla do mediany. Nazwij ją "Poor"
+median_gdp <- quantile(df$GDP, 0.5, na.rm = TRUE)
 # - trzecia kategoria ma zawierać wartości od mediany do trzeciego. Nazwij ją "Medium"
+third_quartile <- quantile(df$GDP, 0.75, na.rm = TRUE)
 # - czwarta kategoria ma zawierać wartości od mediany do trzeciego. Nazwij ją "Rich"
 # - (użyj funkcji ifelse albo case_when)  
+df$GDP_binned <- case_when(
+  df$GDP <= first_quartile ~ "Very_poor", # 0-25
+  df$GDP > first_quartile & df$GDP <= median_gdp ~ "Poor", # 26-50
+  df$GDP > median_gdp & df$GDP <= third_quartile ~ "Medium", # 51-75
+  df$GDP > third_quartile ~ "Rich") # 76-100
 # Stwórz nową zmienną logiczną, która przyjmuje wartość „True”, kiedy wartość 
 # zmiennej „Birthrate” jest większa lub równa od wartości zmiennej „Deathrate”,
 # a wartość „False” w przeciwnym przypadku. Wartość „True” w nowej zmiennej
 # oznacza – najprościej ujmując – że w roku badania w danym kraju więcej osób się
-# urodziło niż umarło. Stwórz wykres słupkowy dla nowo powstałej zmiennej, 
+# urodziło niż umarło. 
+df$More_Births <- df$Birthrate >= df$Deathrate
+#Stwórz wykres słupkowy dla nowo powstałej zmiennej, 
 #  pokazując częstości występowania pierwszej i drugiej kategorii.
+barplot(table(df$More_Births), 
+        main = "Kraje z większą liczbą urodzeń niż zgonów",
+        col = c("lightblue", "lightgreen"), 
+        legend = c("Więcej zgonów", "Więcej urodzeń"),
+        ylab = "Liczba krajów",
+        xlab = "Więcej urodzeń niż zgonów")
+
